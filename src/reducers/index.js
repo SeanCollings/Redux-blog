da50cb1dd984d362ea095c21f9fb7e30fb0c3ced
collingss <nightharrier@gmail.com>
2017-12-22 12:37:50 +0200

    Added redux-from validation and Cancel button

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer // Set it as 'form' since the rest of the app expects this
 });
 
 export default rootReducer;
